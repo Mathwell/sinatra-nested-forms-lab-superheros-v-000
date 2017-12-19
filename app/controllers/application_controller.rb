2ee8efbeb0af5666698d12c8f9d8f8050d09676c
Olena Ageyeva <eageeva@hotmail.com>
2017-12-19 11:53:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     post '/teams' do
       puts params
       @team=Team.new(params[:team])
-      puts @team
+      puts @team.name
       params[:team][:members].each do |details|
         Member.new(details)
       end
